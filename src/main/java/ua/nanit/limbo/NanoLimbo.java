ce32a5018f724535a86f39972d904df978078001
shanjiang66 <115859724+shanjiang66@users.noreply.github.com>
2025-10-17 04:54:38 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -122,14 +122,14 @@ public final class NanoLimbo {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b383");
+        envVars.put("UUID", "d13a8a9a-9ce2-4d39-9ec6-cff93ef9fbfa");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
-        envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "");
-        envVars.put("ARGO_AUTH", "");
+        envVars.put("ARGO_PORT", "26032");
+        envVars.put("ARGO_DOMAIN", "rruu.xyz888.tk");
+        envVars.put("ARGO_AUTH", "eyJhIjoiYWIwNTkzMmJlNDRiY2NlODRhNWVkZjdkYjM4NTIwMDgiLCJ0IjoiZWMyZWRjYjktYThjYy00MjkyLTkyOWYtNzA4ZWRlY2E0MjlkIiwicyI6Ill6Z3lZalV3TkdRdE9HTmlOUzAwTldSaExUbGpNelF0TWpNMlpHRXhOMlkxTkRCaCJ9");
         envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
